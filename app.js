3ce6d5cbb63773a0f7ceef1d2642e9bcbdfe5812
Nicolas Garcia Rosell <nicolasgarciarosell@nicolass-air.home>
2021-08-08 12:15:52 -0400

    Add gitignore, procfile, and update ports

--- app.js
+++ app.js
@@ -170,6 +170,15 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+// Heroku Port
+// This allows us to run our "node server" online and/or locally
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
